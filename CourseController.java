a383047ed6522c84791c7e8ec6fe751c06c46f95
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2011-12-19 20:46:43 +0200

    CourseController add method

--- CourseController.java
+++ CourseController.java
@@ -1,11 +1,45 @@
 package SystemEducation;
 
+import java.util.LinkedList;
+
 /**
  * @author bassem
  * @version 1.0
  * @created 10-����������-2011 08:03:11 �
  */
 public class CourseController {
+        LinkedList<Student> Students;
+        LinkedList<AcademicStaff> AcademicStaffs;    
+        LinkedList<Supervisor> Supervisors;   
+        LinkedList<Course> Courses;   
+        LinkedList<Semester> Semesters;   
+        LinkedList<Enrollement> Enrollements;           
+    
+        public boolean add (Object object) {
+            
+            if (object instanceof Student)
+                Students.add((Student)object);
+            
+            else if (object instanceof AcademicStaff)
+                AcademicStaffs.add((AcademicStaff)object);
+            
+            else if (object instanceof Supervisor)
+                Supervisors.add((Supervisor)object);
+            
+            else if (object instanceof Course)
+                Courses.add((Course)object);
+            
+            else if (object instanceof Semester)
+                Semesters.add((Semester)object);
+            
+            else if (object instanceof Enrollement)
+                Enrollements.add((Enrollement)object);
+            
+            else 
+                return false;            
+        
+            return true;
+        }
 
 	public CourseController(){
 
